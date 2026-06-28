ceb3898db4dd520177f0b1dd8ec1c14bff573194
lovemin1401-debug <lovemin1401@gmail.com>
2026-06-29 07:49:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
 fetch("footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
     });
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("active");
+}
+document.addEventListener("click", function (e) {
+
+    if (e.target.classList.contains("dropdown-btn")) {
+        e.target.parentElement.classList.toggle("open");
+    }
+
+});
