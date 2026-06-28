20067bd1a4231918119cd8cc544272a80f79305d
lovemin1401-debug <lovemin1401@gmail.com>
2026-06-29 07:54:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+// Header 불러오기
 fetch("header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
+
+        // 햄버거 메뉴
+        window.toggleMenu = function () {
+            document.getElementById("navMenu").classList.toggle("active");
+        };
+
+        // 드롭다운 메뉴
+        document.querySelectorAll(".dropdown-btn").forEach(function(button) {
+            button.addEventListener("click", function() {
+                this.parentElement.classList.toggle("open");
+            });
+        });
+
     });
+
+// Footer 불러오기
 fetch("footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
     });
-function toggleMenu() {
-    document.getElementById("navMenu").classList.toggle("active");
-}
-document.addEventListener("click", function (e) {
-
-    if (e.target.classList.contains("dropdown-btn")) {
-        e.target.parentElement.classList.toggle("open");
-    }
-
-});
